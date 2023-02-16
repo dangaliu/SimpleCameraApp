a790b7a860720eda31ef447c9392484e2a442505
dangaliu <ii4603399@gmail.com>
2023-02-16 18:56:36 +0300

    Добавил PreviewView

--- app/src/main/java/com/example/simplecameraapp/view/MainActivity.kt
+++ app/src/main/java/com/example/simplecameraapp/view/MainActivity.kt
@@ -5,8 +5,13 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.example.simplecameraapp.databinding.ActivityMainBinding
 import com.example.simplecameraapp.viewmodel.MainViewModel
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private val TAG = "MainActivity"
 
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
     private var requestAppPermissionsLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
             viewModel.allPermissionsGranted = true
-            for ((permission, isGranted) in map) {
+            for ((permission, isGranted) in it) {
                 if (!isGranted) viewModel.allPermissionsGranted = false
             }
             if (!viewModel.allPermissionsGranted) {
@@ -35,9 +42,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (viewModel.checkAppPermissions()) {
-
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                bindPreview(cameraProvider)
+            }, ContextCompat.getMainExecutor(this))
         } else {
             requestAppPermissionsLauncher.launch(viewModel.appRequestList)
         }
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder().build()
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        cameraProvider.unbindAll()
+        val camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
     }
 }
